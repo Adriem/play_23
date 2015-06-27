6ae632b167463145d0b7323d7de5e3ff557cacf3
Adrian Reyes <sadrian.reyes@gmail.com>
2015-06-27 15:01:07 +0200

    CORS Enabled

--- app/Global.scala
+++ app/Global.scala
@@ -3,12 +3,27 @@ import play.api.db.DB
 import play.api.libs.json.Json
 import play.api.{Logger, Application, GlobalSettings}
 
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.Play.current
+import play.api.mvc._
 
 import scala.io.Source
 import scala.slick.driver.MySQLDriver.simple._
 
-object Global extends GlobalSettings{
+class CorsFilter extends EssentialFilter {
+  def apply(next: EssentialAction) = new EssentialAction {
+    def apply(requestHeader: RequestHeader) = {
+      next(requestHeader).map { result =>
+        result.withHeaders("Access-Control-Allow-Origin" -> "*",
+          "Access-Control-Expose-Headers" -> "WWW-Authenticate, Server-Authorization",
+          "Access-Control-Allow-Methods" -> "POST, GET, OPTIONS, PUT, DELETE",
+          "Access-Control-Allow-Headers" -> "x-requested-with,content-type,Cache-Control,Pragma,Date")
+      }
+    }
+  }
+}
+
+object Global extends WithFilters(new CorsFilter) with GlobalSettings{
 
   implicit val beerFormat = Json.format[Beer]
 
@@ -23,4 +38,7 @@ object Global extends GlobalSettings{
         }
     }
   }
+
+
+
 }
